7c305567d4e762415dec081365589ef9f6255579
Frigo <eduardohdezf@gmail.com>
2019-04-04 21:05:54 +0200

    FirstCommit

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+
+    class Program
+    {
+        public delegate void WorkPerformedHandler(int hours, WorkType workType);
+
+        static void Main(string[] args)
+        {
+            //WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            //WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            //WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
+
+            //del1 += del2;
+            //del1 += del3;
+
+            //del1(10, WorkType.GenerateReports);
+
+            var worker = new Worker();
+            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
+            worker.WorkCompleted += new EventHandler(Worker_WorkCompleted);
+
+            worker.DoWork(8, WorkType.GenerateReports);
+
+            Console.Read();
+        }
+
+        private static void Worker_WorkCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Worker is Done");
+        }
+
+        private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+        }
+
+        //static void DoWork(WorkPerformedHandler del)
+        //{
+        //    del(5, WorkType.Golf);
+        //}
+
+        //static void WorkPerformed1(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine("WorkPerformed1 called " + hours.ToString());
+        //}
+
+        //static void WorkPerformed2(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine("WorkPerformed2 called " + hours.ToString());
+        //}
+
+        //private static void WorkPerformed3(int hours, WorkType workType)
+        //{
+        //    Console.WriteLine("WorkPerformed3 called " + hours.ToString());
+        //}        
+    }
+}
